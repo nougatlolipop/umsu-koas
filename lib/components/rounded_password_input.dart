8abc7df9cbcac07701f0508f19b4ae103dc2b7c8
irvan <august.ivan20@gmail.com>
2021-12-29 20:51:12 +0700

    add all screen

--- lib/components/rounded_password_input.dart
+++ lib/components/rounded_password_input.dart
@@ -3,7 +3,7 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import '../constants.dart';
 import 'input_container.dart';
 
-class RoundedPasswordInput extends StatelessWidget {
+class RoundedPasswordInput extends StatefulWidget {
   const RoundedPasswordInput({Key key, @required this.hint, this.name})
       : super(key: key);
 
@@ -11,19 +11,37 @@ class RoundedPasswordInput extends StatelessWidget {
   final name;
 
   @override
+  State<RoundedPasswordInput> createState() => _RoundedPasswordInputState();
+}
+
+class _RoundedPasswordInputState extends State<RoundedPasswordInput> {
+  bool _isObscure = true;
+  @override
   Widget build(BuildContext context) {
     return InputContainer(
         child: TextField(
-      controller: name,
+      controller: widget.name,
       cursorColor: kPrimaryColor,
-      obscureText: true,
+      obscureText: _isObscure,
       decoration: InputDecoration(
+        icon: Icon(
+          MdiIcons.lockOutline,
+          color: kPrimaryColor,
+        ),
+        hintText: widget.hint,
+        border: InputBorder.none,
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              _isObscure = !_isObscure;
+            });
+          },
           icon: Icon(
-            MdiIcons.lockOutline,
+            _isObscure ? MdiIcons.eyeOutline : MdiIcons.eyeOffOutline,
             color: kPrimaryColor,
           ),
-          hintText: hint,
-          border: InputBorder.none),
+        ),
+      ),
     ));
   }
 }
