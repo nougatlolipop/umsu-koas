b18060d708e4bee05ca970736bbc0faa69a4f128
archy7seven <august.ivan20@gmail.com>
2022-01-07 16:30:09 +0700

    add WYSIWYG belum fix notus file

--- lib/screen/logbook/logbookScreen.dart
+++ lib/screen/logbook/logbookScreen.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:html_editor_enhanced/html_editor.dart';
 import 'components/body.dart';
 
-class LogBookScreen extends StatefulWidget {
+class LogBookScreen extends StatelessWidget {
   static String routeName = '/logbookScreen';
 
-  @override
-  State<LogBookScreen> createState() => _LogBookScreenState();
-}
-
-class _LogBookScreenState extends State<LogBookScreen> {
   @override
   Widget build(BuildContext context) {
     return Body();
